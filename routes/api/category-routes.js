a08411dafb19da6aeb22c0567db49e4f12d5fd62
Craig <craig.skicko@gmail.com>
2022-08-17 18:46:07 +1000

    Added route to handle deleting categories by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,8 +35,18 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteReturnVal = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deleteReturnVal);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
