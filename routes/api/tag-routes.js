2160683b585f43749cb125d0782c64c414d447ba
Craig <craig.skicko@gmail.com>
2022-08-17 17:44:50 +1000

    Added route to handle creation of new tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,8 +29,29 @@ router.get('/:id', async (req, res) => {
   };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tag = await Tag.create(req.body);
+  
+    // If product ids are passed as part of the request body then add the ids to the ProductTag model/table
+    if (req.body.products.length) {
+      const prodTagArray = req.body.products.map((product_id) => {
+        return {
+          product_id,
+          tag_id: tag.id,
+        };
+      });
+
+      const createdLinks = await ProductTag.bulkCreate(prodTagArray);
+      res.status(200).json([tag, createdLinks]);
+    } else {
+      res.status(200).json(tag);
+    }
+
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
 router.put('/:id', (req, res) => {
