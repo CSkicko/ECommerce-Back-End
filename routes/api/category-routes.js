10b3ce852a9f6c2f5e1e11a241a76958a5f6af1b
Craig <craig.skicko@gmail.com>
2022-08-17 18:59:42 +1000

    Added routes to handle creating and updating categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,12 +27,28 @@ router.get('/:id', async (req, res) => {
   };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateReturnVal = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updateReturnVal);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
