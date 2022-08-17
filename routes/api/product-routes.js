dd2c943cad223b653aa0ecd42ed07fc45f0a5068
Craig <craig.skicko@gmail.com>
2022-08-17 17:18:30 +1000

    Added a route to delete a product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -104,8 +104,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteReturnVal = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deleteReturnVal);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
